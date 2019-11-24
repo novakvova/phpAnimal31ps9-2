84899ce1445aeda8e05a75b4ba9cbcfd9fe54dd3
novakvova <novakvova@gmail.com>
2019-11-24 11:57:16 +0200

    Add footer and scripts

--- animals.php
+++ animals.php
@@ -3,3 +3,19 @@ include_once "_header.php";
 include_once "_navbar.php";
 ?>
 
+<div class="container">
+    <div class="row">
+        <h1>Тваринки</h1>
+        <?php
+        $a = 12;
+        $b = 12;
+        $c = $a + $b;
+        echo "Hello Apache"; ?>
+    </div>
+</div>
+
+
+<?php
+include_once "_scripts.php";
+include_once "_footer.php";
+?>
--- index.php
+++ index.php
@@ -14,8 +14,8 @@ include_once "_navbar.php";
         </div>
     </div>
 
-    <script src="/js/jquery-3.3.1.min.js"></script>
-    <script src="/js/popper.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-</body>
-</html>
+
+<?php
+include_once "_scripts.php";
+include_once "_footer.php";
+?>
